85141a1aaf245788ebcdd9348101702468151127
AdmiMed <admimed2202@gmail.com>
2022-07-07 20:41:42 -0500

    carga de archivos

---
+++ js/navegacion.js
@@ -0,0 +1,30 @@
+export function
+  muestraEquipos() {
+  location.href =
+    "equipos.html";
+}
+
+export function
+  muestraJugadores() {
+  location.href = "jugadores.html";
+}
+export function
+  muestraSanciones() {
+  location.href = "sanciones.html";
+}
+
+export function
+  muestraAvisos() {
+  location.href = "avisos.html";
+}
+
+export function
+  muestraArbitros() {
+  location.href = "arbitros.html";
+}
+
+export function
+  muestraDelegados() {
+  location.href = "delegados.html";
+}
+
